a7c204d7862b5ee5c7377ac227ebea05c79eaf8e
CamilaGon <cg561351@gmail.com>
2025-03-07 22:17:58 -0300

    Corrigi margem da pagina

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,22 @@ import React from "react";
 import Header from "./components/Header";
 import FloatingContact from "./components/FloatingContact";
 import Profile from "./components/Profile";
-import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import SocialPreview from "./components/SocialPreview";
-import Summary from "./components/Summary"; 
+import Summary from "./components/Summary";
 
 function App() {
   return (
-    <>
-      <Header />
     <div className="pages">
-      <Profile />
-      <Projects />
-      <SocialPreview />
-      <FloatingContact />
-    </div>
+      <Header />
+      <div className="content">
+        <Profile />
+        <Projects />
+        <SocialPreview />
+        <FloatingContact />
+      </div>
       <Summary />
-    </>
+    </div>
   );
 }
 
